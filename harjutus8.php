fb73146376038263a8b33ad272095cef2b0bc966
Anneliloo <anneli.loo94@gmail.com>
2014-10-15 10:26:39 +0300

    initial commit

---
+++ harjutus8.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Harjutus-8</title>
+    <meta http-equiv="Content-Type" content="text/html;
+    charset=utf-8">
+    <title>PHP põhitõed</title>
+</head>
+<body>
+   	<h2>Funktsioon "continue"</h2>
+   		<?php
+    		for ($number = 1; $number <= 10; $number++) {
+    			if ($number == 5) {
+       			 	continue;
+    			}
+    			echo $number . ", ";
+    		}
+		?>
+		<?php
+    		//while ($number = 1; $number <= 10; $number++) {
+    			//if ($number == 5) {
+       			 	//continue;
+    			//}
+				//echo $number . ", ";
+    			//}
+		?>
+	<h2>Funktsioon "break"</h2>
+		<?php
+    		for ($number = 1; $number <= 10; $number++) {
+    			if ($number == 6) {
+       			 	break;
+    			}
+				echo $number . ", ";
+    			}
+		?>
+</body>
+</html>
